870d8f826a0c1828e4dafafbeb41f4cb35eafc0d
isabellapresto <isabella.presto@gmail.com>
2023-03-29 10:43:08 +0200

    contact done

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname){
 for(tablink of tablinks){
-  tablink.classList.remove ("active-link")
+ tablink.classList.remove ("active-link")
 }
 
 for(tabcontent of tabcontents){
-  tabcontent.classList.remove ("active-tab")
+ tabcontent.classList.remove ("active-tab")
 }
 
 event.currentTarget.classList.add("active-link");
